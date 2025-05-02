df90845dc429bff4d3344bedbb1e1e47315a3765
Ledonic <ledonic7@gmail.com>
2025-05-02 22:56:51 +0400

    Reported charts + Update events

--- data/stress-(pico-mix)/script.lua
+++ data/stress-(pico-mix)/script.lua
@@ -1,12 +1,12 @@
-function onCreate()
-    precacheImage('characters/masks/tankmanCaptainBloody_mask')
+function onCreatePost()
+    setShaderSampler2D('dad', 'altMask', 'characters/masks/tankmanCaptainBloody_mask')
+	setShaderFloat('dad', 'thr2', 1)
+	setShaderBool('dad', 'useMask', false)
 end
 
 function onEvent(event, value1, value2, strumTime)
     if event == 'Change Character' then
-        setShaderSampler2D('dad', 'altMask', 'characters/masks/tankmanCaptainBloody_mask')
-		setShaderFloat('dad', 'thr2', 1)
-		setShaderBool('dad', 'useMask', true)
+        setShaderBool('dad', 'useMask', true)
         callMethod('iconP2.changeIcon', {'tankman'})
         callMethod('set_health', {getProperty('health')})
     end
